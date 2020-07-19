118c092358ac1ea05506e7a75b1107249cc724d8
Christopher Gammie <contact@gammie.co.uk>
2020-07-19 16:39:08 +0100

    Add some basic filters

---
+++ src/Eloquent/Filters/Scope.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Copyright 2020 Cloud Creativity Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace LaravelJsonApi\Eloquent\Filters;
+
+use LaravelJsonApi\Core\Support\Str;
+use LaravelJsonApi\Eloquent\Contracts\Filter;
+
+class Scope implements Filter
+{
+
+    use Concerns\DeserializesValue;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $scope;
+
+    /**
+     * Scope constructor.
+     *
+     * @param string $name
+     * @param string|null $scope
+     */
+    public function __construct(string $name, string $scope = null)
+    {
+        $this->name = $name;
+        $this->scope = $scope ?: $this->guessScope();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function key(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function apply($query, $value)
+    {
+        return $query->{$this->scope}(
+            $this->deserialize($value)
+        );
+    }
+
+    /**
+     * @return string
+     */
+    private function guessScope(): string
+    {
+        return Str::camel($this->name);
+    }
+}
